42151af3f7836829c21790c19e4537335566cb5f
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-07-24 16:22:01 +0900

    refactor, feat: change directory, attach camera to player

--- madcamp-week4/Assets/Scripts/Camera/CameraFollow.cs
+++ madcamp-week4/Assets/Scripts/Camera/CameraFollow.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
     public float smoothSpeed = 2f;
     public Vector3 positionOffset;
     public Vector3 lookOffset;
 
+    Transform target;
+
+    private void Start()
+    {
+        target = transform.parent;
+    }
+
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + target.TransformDirection(positionOffset);
+        Vector3 desiredPosition = transform.parent.position + target.TransformDirection(positionOffset);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
         transform.LookAt(target.position + target.TransformDirection(lookOffset));
